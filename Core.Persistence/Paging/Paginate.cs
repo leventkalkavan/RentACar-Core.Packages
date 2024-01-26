ac5a2006d039ad117af21944c57fba52b445bd90
leventkalkavan <levent.kalkavan@hotmail.com>
2024-01-26 23:11:58 +0300

    Initial commit

---
+++ Core.Persistence/Paging/Paginate.cs
@@ -0,0 +1,16 @@
+namespace Core.Persistence.Paging;
+
+public class Paginate<T>
+{
+    public Paginate()
+    {
+        Items = Array.Empty<T>();
+    }
+
+    public int Size { get; set; }
+    public int Index { get; set; }
+    public int Count { get; set; }
+    public int Pages { get; set; }
+    public bool HasPrevious => Index > 0;
+    public IList<T> Items { get; set; }
+}
---
+++ Core.Persistence/Repositories/BaseEntity.cs
@@ -0,0 +1,19 @@
+namespace Core.Persistence.Repositories;
+
+public class BaseEntity<TId>: IEntityTimestamps
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+
+    public BaseEntity()
+    {
+        Id = default;
+    }
+
+    public BaseEntity(TId id)
+    {
+        Id = id;
+    }
+}
